423acf9b1f18ca582e1bedb829534a4c53d623a9
Redice1997 <roshin-vladimir@mail.ru>
2024-01-18 03:13:14 +0300

    remove test console project

---
+++ TestConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GeometryLibrary;
+using GeometryLibrary.Models;
+
+var t = new Triangle(1, 2, 2);
+var x = Geometry.CreateTriangle(1, 2, 2).Perimeter;
+var z = Geometry.GetPerimeter(t);
+
+Console.WriteLine(x);
+Console.WriteLine(z);
+Console.WriteLine(t.IsRectangular);
+Console.WriteLine(Geometry.CreateTriangle(3, 4, 5).IsRectangular);
